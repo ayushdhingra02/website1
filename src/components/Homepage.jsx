20bca9b5275d1944b3b1162026de6c105921100b
Ayush Dhingra <98825315+ayushdhingra02@users.noreply.github.com>
2023-07-26 10:15:15 +0530

    Update Homepage.jsx

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -37,17 +37,7 @@ function Homepage() {
   };
 
   return (
-    <form>
-      <label htmlFor="item">Select an item:</label>
-      <select id="item" value={selectedItem} onChange={handleItemChange}>
-        <option value="">Select</option>
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </select>
-
-      {renderDynamicField()}
-    </form>
+    <></>
   );
 }
 
